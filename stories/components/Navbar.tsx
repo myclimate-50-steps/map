84acaeeb6dc7fab0521b2b3b43aec6aed0d6d201
Leon Babic <leon@leonbabic.ch>
2022-05-12 17:06:20 +0200

    💄 Change css to modular

--- stories/components/Navbar.tsx
+++ stories/components/Navbar.tsx
@@ -1,30 +1,30 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import React from "react";
-import "./Navbar.css";
+import styles from "./Navbar.module.css";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface NavbarProps {}
 
 export const Navbar = ({}: NavbarProps) => {
   return (
-    <nav className="navbar-container">
-      <ul className="nav navbar">
-        <li className="nav-item">
+    <nav className={styles.navbarContainer}>
+      <ul className={`${styles.nav} ${styles.navbar}`}>
+        <li className={styles.navItem}>
           <a href="/">HOME</a>
         </li>
-        <li className="nav-item">
+        <li className={styles.navItem}>
           <a href="/project/">UNSER PROJEKT</a>
         </li>
-        <li className="nav-item active">
+        <li className={`${styles.navItem} ${styles.active}`}>
           <a href="https://myclimate-50-steps.github.io/map/">MAP</a>
         </li>
-        <li className="nav-item">
+        <li className={styles.navItem}>
           <a href="#">KONTO</a>
         </li>
-        <li className="nav-item">
+        <li className={styles.navItem}>
           <a href="/about/">ÜBER UNS</a>
         </li>
-        <li className="nav-item">
+        <li className={styles.navItem}>
           <a href="#">KONTAKT</a>
         </li>
       </ul>
