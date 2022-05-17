07c1a8f4cbc1a921ede0580e37ad5aca5f5036a4
Leon Babic <leon@leonbabic.ch>
2022-05-17 22:19:51 +0200

    🩹 Fix Tailwind implementation

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,6 +10,14 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
 };
